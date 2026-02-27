878bcfd340e4939d4589f76413298b362f235ee8
medamineaitaich <medamineaitaich@gmail.com>
2026-02-27 16:31:36 +0000

    Make /api/ping show env status (safe)

--- api/ping.js
+++ api/ping.js
@@ -4,5 +4,7 @@ module.exports = (req, res) => {
     wc_url: process.env.WC_URL || null,
     has_key: !!process.env.WC_KEY,
     has_secret: !!process.env.WC_SECRET,
+    vercel_env: process.env.VERCEL_ENV || null,
+    vercel_url: process.env.VERCEL_URL || null
   });
 };
