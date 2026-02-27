4bc13e4fa0e9d3064267864ffe54cd8475419f58
medamineaitaich <medamineaitaich@gmail.com>
2026-02-27 16:47:25 +0000

    chore: update ping handler cache control

--- api/ping.js
+++ api/ping.js
@@ -1,10 +1,11 @@
-module.exports = (req, res) => {
+export default function handler(req, res) {
+  res.setHeader("Cache-Control", "no-store");
   res.status(200).json({
     ok: true,
     wc_url: process.env.WC_URL || null,
     has_key: !!process.env.WC_KEY,
     has_secret: !!process.env.WC_SECRET,
     vercel_env: process.env.VERCEL_ENV || null,
-    vercel_url: process.env.VERCEL_URL || null
+    vercel_url: process.env.VERCEL_URL || null,
   });
-};
+}
