5d22235d43ed25e3055ae766c2543e14ea98d4a7
medamineaitaich <medamineaitaich@gmail.com>
2026-02-27 16:21:42 +0000

    chore: update ping env diagnostics

--- api/ping.js
+++ api/ping.js
@@ -1,4 +1,8 @@
-export default function handler(req, res) {
-  // Deployment trigger no-op endpoint
-  res.status(200).json({ ok: true, message: "ping works" });
-}
+module.exports = (req, res) => {
+  res.status(200).json({
+    ok: true,
+    wc_url: process.env.WC_URL || null,
+    has_key: !!process.env.WC_KEY,
+    has_secret: !!process.env.WC_SECRET,
+  });
+};
